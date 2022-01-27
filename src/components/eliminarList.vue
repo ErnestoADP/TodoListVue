d886f518d59176f2b853ff5104dc7c36d8b5ab7f
Ernesto Diaz <ernesto.adp1@gmail.com>
2022-01-27 01:33:56 -0300

    segunda parte desafio prop

---
+++ src/components/eliminarList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div >
+        <ul>
+         <li v-for="(tarea, i) in tareas" :key="i">
+                    {{tarea}}
+           <button @click="eliminarTarea(i)">Eliminar</button>   
+         </li>
+         
+        </ul>
+     
+       
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'eliminarList',
+  props: {
+    tareas:[],
+  },
+    methods:{
+      
+        eliminarTarea(index){
+           this.$emit("eliminarTarea", index)
+        },
+    },
+}
+
+
+
+
+</script>
--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -1,38 +1,44 @@
 <template>
     <div id="aqui">
-        <h1>Lista de tareas</h1>
+        <h1>Lista de tareas</h1> 
         <input v-model="tarea">
         <button @click="addTarea">Agregar</button>
          <h1>Lista</h1>
-         <ul>
-             <li v-for="(tarea, i) in tareas"
-           :key="i">
-                {{tarea}}
-             </li>
-         </ul>
+      <section v-if="tareas.length > 0">
+                <eliminarList :tareas="tareas" @eliminarTarea="eliminarTarea"/>
+      </section>
     </div>
 </template>
 
 
 <script>
+import eliminarList from "@/components/eliminarList.vue";
+
+
 export default {
+ 
   name: 'todoList',
+  components:{
+      eliminarList,
+  },
    data() {
        return {
          tarea: "",
-        tareas:[]
+        tareas:[],
     };
     
     },
     methods:{
         addTarea(){
-            const tarea = this.tarea;
+            const {tarea} = this;
             this.tareas.push(tarea);
-        }
-    }
+        },
+          eliminarTarea(indexTarea){
+           this.tareas.splice(indexTarea, 1);
+        },
+      
+    },
 }
-
-
 
 
 </script>
